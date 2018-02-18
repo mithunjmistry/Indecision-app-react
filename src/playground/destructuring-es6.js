290a9d38329b28f1339f84603d5557117ce37ecf
Mithun <mithunjmistry@gmail.com>
2018-02-18 16:26:51 -0500

    destructuring array es6

--- src/playground/destructuring-es6.js
+++ src/playground/destructuring-es6.js
@@ -18,3 +18,14 @@ const {temp: temperature = 72, city} = person.location;
 
 console.log(`${name} is ${age} year(s) old.`);
 console.log(`Temperature is ${temperature} degrees in ${city}.`);
+
+// array destructuring
+
+const address = ['320 Mallard Creek', 'Charlotte', 'North Carolina', '28080'];
+
+// object destructuring happens with {} but array destructuring happens with []
+// it is destructuring on index number
+// skip the item we don't want with a comma, set default value with =
+
+const [, , state = 'California', zip] = address;
+console.log(`I am in ${state}.`);
